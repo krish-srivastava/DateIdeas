b8b1f9b8214eaeb3e79781124ee44661ee8b1af3
Krishna Srivastava <krishna.srivastava@students.unsw.edu.au>
2024-05-19 19:06:39 +1000

    Pushing to online repo to continue working from laptop

--- main.c
+++ main.c
@@ -6,40 +6,34 @@
 
 int main(void) {
 
-
     // Introductory message. 
-
     char choice;
-
-    printf("Welcome to Date Night Ideas");
+    printf("Welcome to Date Night Ideas\n");
 
     // Scan the choice being selected
+    printf("Please enter your option\n");
+    printf("e - Log the amount of times you have eaten outside\n");
     scanf("%c", &choice);
     char eatTimes = 'e'; 
     char eatChoice = 'c';
     
-    int amountTimes = 0;
+    static int amountTimes = 0; 
     int maxAmountTimes = 2;
-    
-    // Input the amount of times eating out has occurred. 
-    // Set letter for inputting amount of times eating has occurred. 
-        if (choice.isEquals(eatTimes)) {
-            if (amountTimes.isEquals(maxAmountTimes)) {
-                printf("You have eaten enough times this week!\n");
-                return 1; 
-            }
-            amountTimes++;
+
+    if (choice == eatTimes) {
+        printf("%d\n", amountTimes);
+        if (amountTimes == maxAmountTimes) {
+            printf("You have eaten enough times this week!\n");
+            return 1;
         }
-        else if (choice.isEquals(eatChoice)) {
-            
-        }
-    
-
-        // Store into an array to note down the amount of times something has been eaten. 
-    
-    
-    scanf()
-
-    return 0; 
-
+        amountTimes++;
+    }
+    else if (choice == eatChoice) {
+        printf("This feature is still being built out, please be patient!");
+    }
+    else {
+        printf("Please enter a valid option");
+    }
+        
+    return 0;
 }
